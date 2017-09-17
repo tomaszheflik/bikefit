b98ebaa9fd757639a9e05b872dec8b5deb1aed68
Tomasz Heflik <heflik@gmail.com>
2017-09-17 22:25:07 +0200

    test code

--- video-engine/main.go
+++ video-engine/main.go
@@ -6,11 +6,26 @@ import (
 )
 
 func main() {
-	pointV10 := v.Point{1, 1}
-	pointV11 := v.Point{10, 10}
-	vector, err := v.GetVector(pointV10, pointV11)
-	if err != nil {
-		fmt.Printf("Got error %+v" err)
+	start1 := v.Point{10, 10}
+	stop1 := v.Point{30, 10}
+	vector1, err1 := v.GetVector(start1, stop1)
+
+	start2 := v.Point{10, 10}
+	stop2 := v.Point{10, 30}
+	vector2, err2 := v.GetVector(start2, stop2)
+
+
+
+	if err1 != nil || err2 != nil {
+		fmt.Printf("Got error %+v, %+v", err1, err2)
 	}
-	fmt.Printf("Vector from points: %+v", vector)
-	}
+
+	fmt.Printf("Vectors from points V1: %+v V2: %+v\n", vector1, vector2)
+	lenght1 := v.VectorLenght(vector1)
+	lenght2 := v.VectorLenght(vector2)
+
+	fmt.Printf("Lenght: %+v %+v\n", lenght1, lenght2)
+	ang1,ang2 := v.GetAngel(vector1, vector2)
+	fmt.Printf("RAD: %+v, DEG: %+v\n", ang1, ang2)
+
+}
