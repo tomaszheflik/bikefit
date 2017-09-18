0dea7fce9e33163faf812257afe29b48f21458a8
Tomasz Heflik <heflik@gmail.com>
2017-09-18 11:50:23 +0200

    Use calculate function

--- video-engine/main.go
+++ video-engine/main.go
@@ -11,20 +11,13 @@ func main() {
 	vector1, err1 := v.GetVector(start1, stop1)
 
 	start2 := v.Point{10, 10}
-	stop2 := v.Point{10, 30}
+	stop2 := v.Point{30, 30}
 	vector2, err2 := v.GetVector(start2, stop2)
-
-
 
 	if err1 != nil || err2 != nil {
 		fmt.Printf("Got error %+v, %+v", err1, err2)
 	}
 
-	fmt.Printf("Vectors from points V1: %+v V2: %+v\n", vector1, vector2)
-	lenght1 := v.GetLenght(vector1)
-	lenght2 := v.GetLenght(vector2)
-
-	fmt.Printf("Lenght: %+v %+v\n", lenght1, lenght2)
 	ang1,ang2 := v.GetAngel(vector1, vector2)
 	fmt.Printf("RAD: %+v, DEG: %+v\n", ang1, ang2)
 
