4e9ee27858b2f7cb5b7d92f1e079d0a0d94aad2b
Tomasz Heflik <heflik@gmail.com>
2017-10-11 19:23:06 +0200

    Add median blur to loaded file

--- test-data/image_test.py
+++ test-data/image_test.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+import cv2
 import numpy as np
 import sys
 
@@ -9,7 +9,7 @@ color_span = int(sys.argv[4])
 image_to_load = sys.argv[5]
 
 color = np.uint8([[[blue, green, red]]])
-hsv_color = cv.cvtColor(color, cv.COLOR_BGR2HSV)
+hsv_color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
 hue = hsv_color[0][0][0]
 print ("Hue: " + str(hue) + ", Color span: " + str(color_span))
 
@@ -22,18 +22,17 @@ else:
     upper_hue = hue
     print ("Lower Hue: " + str(lower_hue) + ", Upper Hue: " + str(upper_hue))
 
-image = cv.imread(image_to_load)
-if image is None:
+image_r = cv2.imread(image_to_load)
+if image_r is None:
     print ("No image readed")
     exit()
-
-image_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+image = cv2.medianBlur(image_r, 3)
+image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 lower_color = np.array([lower_hue, 100, 100], dtype=np.uint8)
 upper_color = np.array([upper_hue, 255, 255], dtype=np.uint8)
-mask = cv.inRange(image_hsv, lower_color, upper_color)
+mask = cv2.inRange(image_hsv, lower_color, upper_color)
+res = cv2.bitwise_and(image, image, mask=mask)
 
-res = cv.bitwise_and(image, image, mask=mask)
-
-cv.imwrite("out.jpg", res)
-cv.imwrite("hsv.jpg", image_hsv)
-cv.imwrite("mask.jpg", mask)
+cv2.imwrite("out.jpg", res)
+cv2.imwrite("hsv.jpg", image_hsv)
+cv2.imwrite("mask.jpg", mask)
