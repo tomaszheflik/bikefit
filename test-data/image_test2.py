a888e9b1f447a33a4f18403958cbc11f45a6f0c9
Tomasz Heflik <heflik@gmail.com>
2017-10-13 14:30:44 +0200

    Can print skeleton

--- test-data/image_test2.py
+++ test-data/image_test2.py
@@ -2,24 +2,27 @@ import cv2
 import numpy as np
 import sys
 import imutils
+import datetime
 
-color = sys.argv[1]               # COLOR (red, yellow, blue)
-color_span = int(sys.argv[2])   # SPAN OF COLOR
-image_to_load = sys.argv[3]     # IMAGE TO LOAD
-
+image_to_load = sys.argv[1]     # IMAGE TO LOAD
+DEBUG = False
 # Return hue for given color
 def getHue(rgb, color_span):
     hsv_color = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
     hue = hsv_color[0][0][0]
-    # print ("Hue: " + str(hue) + ", Color span: " + str(color_span))
+    if DEBUG: print ("Hue: " + str(hue) + ", Color span: " + str(color_span))
     if hue >= color_span:
         lower_hue = hue - color_span
         upper_hue = hue + color_span
-        # print ("Lower Hue: " + str(lower_hue) + ", Upper Hue: " + str(upper_hue))
+        if DEBUG: print ("Lower Hue: " + str(lower_hue) + ", Upper Hue: " + str(upper_hue))
+    elif(hue == 0):
+        lower_hue = 0
+        upper_hue = color_span
+        if DEBUG: print ("Lower Hue: " + str(lower_hue) + ", Upper Hue: " + str(upper_hue))
     else:
         lower_hue = hue
         upper_hue = hue
-        # print ("Lower Hue: " + str(lower_hue) + ", Upper Hue: " + str(upper_hue))
+        if DEBUG: print ("Lower Hue: " + str(lower_hue) + ", Upper Hue: " + str(upper_hue))
     hue_return = [lower_hue, upper_hue]
     return hue_return
 
@@ -50,56 +53,89 @@ def getRGB(color):
 
     if(color == "blue"):
         rgb = np.uint8([[[255,0,0]]])
+    if(color == "green"):
+        rgb = np.uint8([[[0,255,0]]])
     return rgb
 
 # Return shape in given color
 def findShapeInColor(frame, color, color_span):
+    t1 = datetime.datetime.now()
+    tmpJoints = {}
     orderedJoints = []
+
     hue = getHue(getRGB(color), color_span)
     image_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_color = np.array([hue[0], 100, 100], dtype=np.uint8)
     upper_color = np.array([hue[1], 255, 255], dtype=np.uint8)
     mask = cv2.inRange(image_hsv, lower_color, upper_color)
     res = cv2.bitwise_and(frame, frame, mask=mask)
-    mask = cv2.GaussianBlur(mask, (3,3),0)
+    mask = cv2.GaussianBlur(mask, (1,1),0)
 
     cv2.imwrite("hsv.jpg", image_hsv)
-    cv2.imwrite("res.jpg", res)
+    # cv2.imwrite("res.jpg", res)
     cv2.imwrite("mask.jpg", mask)
 
-    _, thresh = cv2.threshold(mask,127,255,cv2.THRESH_BINARY)
-    _, contours, _ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    _, thresh = cv2.threshold(mask.copy(),127,255,cv2.THRESH_BINARY)
+    _, contours, _ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
     for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.04*cv2.arcLength(cnt,True),True)
-        if cv2.contourArea(cnt) < 140:
-		continue
-        if len(approx)==5:
-            # print "pentagon"
-            orderedJoints.insert(0, [cnt])
-        elif len(approx)==3:
-            # print "triangle"
-            orderedJoints.insert(0, [cnt])
-        elif len(approx)==4:
-            # print "square"
-            orderedJoints.insert(1, [cnt])
-        elif len(approx) >= 7:
-            # print "circle"
-            orderedJoints.insert(2, [cnt])
+        approx = cv2.approxPolyDP(cnt,0.03*cv2.arcLength(cnt,True),True)
+        if DEBUG:
+            cv2.drawContours(res,[cnt],0,(0,165,255),1)
+            cv2.imwrite("res.jpg", res)
+        if cv2.contourArea(cnt) < 70 or cv2.contourArea(cnt) > 180:
+            if DEBUG: print("Skipping area:" + str(cv2.contourArea(cnt))+ "Approx: " + str(len(approx)))
+            continue
 
-    # print len(orderedJoints)
+        if len(approx)==5:
+            if DEBUG: print("pentagon " + str(len(approx)))
+            tmpJoints['circle'] = cnt
+        elif len(approx)==3:
+            if DEBUG: print("triangle " + str(len(approx)))
+            tmpJoints['triangle'] = cnt
+        elif len(approx)==4:
+            if DEBUG: print("square " + str(len(approx)))
+            tmpJoints['square'] = cnt
+        elif len(approx) >= 7:
+            if DEBUG: print("circle " + str(len(approx)))
+            tmpJoints['circle'] = cnt
+
+    if 'triangle' in tmpJoints: orderedJoints.append(tmpJoints['triangle'])
+    if 'circle' in tmpJoints: orderedJoints.append(tmpJoints['circle'])
+    if 'square' in tmpJoints: orderedJoints.append(tmpJoints['square'])
+    t2 = datetime.datetime.now()
+    timeMetric = t2 - t1
+    print("Video.Frame.Time."+ color + ": " + str(timeMetric))
     return orderedJoints
 
+#
+# MAIN STARY
+#
+
+# Load sinble frame
 frame = openFile(image_to_load)
-hand = findShapeInColor(frame, "red", color_span)
-leg = findShapeInColor(frame, "yellow", color_span)
-foot = findShapeInColor(frame, "blue", color_span)
 
-for c in hand:
-    cv2.drawContours(frame,c,0,(0,255,0),2)
-for c in leg:
-    cv2.drawContours(frame,c,0,(0,255,0),2)
-for c in foot:
-    cv2.drawContours(frame,c,0,(0,255,0),2)
+# Get body componennt by color and build skeleton
+body = []
+joints = []
+for item in findShapeInColor(frame, "green", 4): body.append(item)
+for item in findShapeInColor(frame, "yellow", 2): body.append(item)
+for item in findShapeInColor(frame, "blue", 4): body.append(item)
+if DEBUG: print body
+for item in body:
+    M = cv2.moments(item)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    joints.append([cx,cy])
 
-cv2.imwrite("out.jpg", frame)
+# print joints
+iter = 0
+for joint in joints:
+    if iter > 0:
+        cv2.line(frame, (oldjoint[0],oldjoint[1]), (joint[0],joint[1]), (255,0,0),2)
+    iter=iter+1
+    cv2.circle(frame,(joint[0],joint[1]),10,(0,255,0),1) # draw the outer circle
+    cv2.circle(frame,(joint[0],joint[1]),2,(0,0,255),2) # draw the center of the circle
+    cv2.putText(frame,str(iter), (joint[0]+10,joint[1]+10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (100,255,255))
+    oldjoint = joint
+cv2.imwrite("image.jpg", frame)
